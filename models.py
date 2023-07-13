409e83f712a9a5e7b62f960add8cede07c0cbe75
Eugene Chua <eugenechua96@yahoo.com.sg>
2023-07-14 01:17:38 +0800

    add model

--- models.py
+++ models.py
@@ -12,8 +12,10 @@ import os
 
 print(os.getcwd())
 
+'''
 dir = path.Path(__file__).abspath()
 sys.path.append(dir.parent.parent)
+'''
 
 #initializing language model
 model = SentenceTransformer('all-mpnet-base-v2')
@@ -23,11 +25,11 @@ token_skill_classifier = pipeline(model="jjzha/jobbert_skill_extraction", aggreg
 token_knowledge_classifier = pipeline(model="jjzha/jobbert_knowledge_extraction", aggregation_strategy="first")
 
 #Importing the saved MPNET's embeddings
-embeddings = load('.\skills_embeddings.npy')
+embeddings = load('/app/semantic_skill_extractions/skills_embeddings.npy')
 
 
 #Reading in the deduped skills titles
-df = pd.read_csv('.\skill_master_dedup_06nov2022.csv')
+df = pd.read_csv('/app/semantic_skill_extractions/skill_master_dedup_06nov2022.csv')
 df = df[['skill_id', 'skill_title', 'dup_parent']]
 df['merged_title'] = df['dup_parent'].combine_first(df.skill_title)
 df['source'] = 'skill title'
